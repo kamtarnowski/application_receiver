b062befd655100c665ea66d3c36441af99c09de1
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-07-04 21:54:38 +0200

    Created validations till d point

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,8 @@
 class Submission < ActiveRecord::Base
+
+	validates_presence_of :email, :description, :category
+	validates :description, length: { minimum: 100 }
+	validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+	valdiates :category, inclusion: { in: ['awaria', 'uszkodzenie', 'usterka'] }
+
 end
